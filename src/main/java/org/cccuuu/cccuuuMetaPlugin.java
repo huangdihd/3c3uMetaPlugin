51fc89b5335b0cfc415029a9a087bdbf750fc10a
huangdihd <hd20100104@163.com>
2026-05-30 03:35:03 +0800

    feat: use LoginFlow for auto login instead of manual SessionAdapter

--- src/main/java/org/cccuuu/cccuuuMetaPlugin.java
+++ src/main/java/org/cccuuu/cccuuuMetaPlugin.java
@@ -5,14 +5,19 @@ import org.cccuuu.eventListeners.PublicChatMessageListener;
 import org.cccuuu.listeners.*;
 import org.geysermc.mcprotocollib.protocol.data.game.entity.player.GameMode;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.ClientboundLoginPacket;
+import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.ClientboundSystemChatPacket;
 import xin.bbtt.mcbot.Bot;
 import xin.bbtt.mcbot.Server;
+import xin.bbtt.mcbot.Utils;
+import xin.bbtt.mcbot.LoginFlow.LoginFlow;
 import xin.bbtt.mcbot.plugin.MetaPlugin;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
 public class cccuuuMetaPlugin implements MetaPlugin {
+    private LoginFlow loginFlow;
+
     @Override
     public void onLoad() {
         // LangManager.initLang(cccuuuMetaPlugin.class.getClassLoader());
@@ -24,9 +29,27 @@ public class cccuuuMetaPlugin implements MetaPlugin {
 
     @Override
     public void onEnable() {
-        // Packet listeners
+        loginFlow = LoginFlow.builder(Bot.INSTANCE::sendCommand)
+                .eventManager(Bot.INSTANCE.getPluginManager().events())
+                .templateExpander(t -> t.replace("{password}",
+                        Bot.INSTANCE.getConfig().getConfigData().getAccount().getPassword()))
+                .step(ClientboundSystemChatPacket.class)
+                    .match(p -> Utils.toString(p.getContent()).contains("register"))
+                    .then("reg {password} {password}")
+                    .register()
+                    .skipWhen(p -> Utils.toString(p.getContent()).contains("login"))
+                    .add()
+                .step(ClientboundSystemChatPacket.class)
+                    .match(p -> Utils.toString(p.getContent()).contains("login"))
+                    .then("l {password}")
+                    .login()
+                    .successWhen(p -> Utils.toString(p.getContent()).contains("成功登录"))
+                    .add()
+                .cooldown(2000)
+                .build();
+
+        Bot.INSTANCE.addPacketListener(loginFlow, this);
         Bot.INSTANCE.addPacketListener(new ClientListenerWrapper(), this);
-        Bot.INSTANCE.addPacketListener(new AutoLoginListener(), this);
         Bot.INSTANCE.addPacketListener(new PositionInQueueListener(), this);
         Bot.INSTANCE.addPacketListener(new PingPacketListener(), this);
         Bot.INSTANCE.getPluginManager().events().registerEvents(new PublicChatMessageListener(), this);
