42e21910e5086869206e807769f60af4c7f3f4bc
huangdihd <hd20100104@163.com>
2026-05-30 03:44:17 +0800

    refactor: make loginFlow static final with getter

--- src/main/java/org/cccuuu/cccuuuMetaPlugin.java
+++ src/main/java/org/cccuuu/cccuuuMetaPlugin.java
@@ -1,5 +1,6 @@
 package org.cccuuu;
 
+import lombok.Getter;
 import org.cccuuu.eventListeners.PrivateChatMessageListener;
 import org.cccuuu.eventListeners.PublicChatMessageListener;
 import org.cccuuu.listeners.*;
@@ -16,7 +17,25 @@ import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 
 public class cccuuuMetaPlugin implements MetaPlugin {
-    private LoginFlow loginFlow;
+    @Getter
+    private static final LoginFlow loginFlow = LoginFlow.builder(Bot.INSTANCE::sendCommand)
+            .eventManager(Bot.INSTANCE.getPluginManager().events())
+            .templateExpander(t -> t.replace("{password}",
+                    Bot.INSTANCE.getConfig().getConfigData().getAccount().getPassword()))
+            .step(ClientboundSystemChatPacket.class)
+                .match(p -> Utils.toString(p.getContent()).contains("§3使用以下指令注册账号: /register"))
+                .then("reg {password} {password}")
+                .register()
+                .skipWhen(p -> Utils.toString(p.getContent()).contains("§c使用指令登录: /login"))
+                .add()
+            .step(ClientboundSystemChatPacket.class)
+                .match(p -> Utils.toString(p.getContent()).contains("§c使用指令登录: /login"))
+                .then("l {password}")
+                .login()
+                .successWhen(p -> Utils.toString(p.getContent()).contains("§2§l成功登录!"))
+                .add()
+            .cooldown(2000)
+            .build();
 
     @Override
     public void onLoad() {
@@ -29,25 +48,6 @@ public class cccuuuMetaPlugin implements MetaPlugin {
 
     @Override
     public void onEnable() {
-        loginFlow = LoginFlow.builder(Bot.INSTANCE::sendCommand)
-                .eventManager(Bot.INSTANCE.getPluginManager().events())
-                .templateExpander(t -> t.replace("{password}",
-                        Bot.INSTANCE.getConfig().getConfigData().getAccount().getPassword()))
-                .step(ClientboundSystemChatPacket.class)
-                    .match(p -> Utils.toString(p.getContent()).contains("§3使用以下指令注册账号: /register"))
-                    .then("reg {password} {password}")
-                    .register()
-                    .skipWhen(p -> Utils.toString(p.getContent()).contains("§c使用指令登录: /login"))
-                    .add()
-                .step(ClientboundSystemChatPacket.class)
-                    .match(p -> Utils.toString(p.getContent()).contains("§c使用指令登录: /login"))
-                    .then("l {password}")
-                    .login()
-                    .successWhen(p -> Utils.toString(p.getContent()).contains("§2§l成功登录!"))
-                    .add()
-                .cooldown(2000)
-                .build();
-
         Bot.INSTANCE.addPacketListener(loginFlow, this);
         Bot.INSTANCE.addPacketListener(new ClientListenerWrapper(), this);
         Bot.INSTANCE.addPacketListener(new PositionInQueueListener(), this);
